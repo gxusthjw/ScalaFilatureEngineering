bb7fad42798fc62862ed2e4fec89399f9b4ab015
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-12-03 22:53:34 +0800

    update

--- src/test/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/LogisticTest.scala
+++ src/test/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/LogisticTest.scala
@@ -15,6 +15,41 @@
  */
 package cn.edu.gxust.jiweihuang.scala.math.function.univariate
 
-class LogisticTest {
+import cn.edu.gxust.jiweihuang.scala.math.NumericalConstants
+import cn.edu.gxust.jiweihuang.scala.test.UnitSpec
+
+class LogisticTest extends UnitSpec {
+
+  "a Logistic" should "throw new IllegalArgumentException if {logisticM = 0}" in {
+    assertThrows[IllegalArgumentException] {
+      Logistic(0, 2, 3)
+    }
+  }
+
+  it should "not throw new IllegalArgumentException if {logisticM = 0}" in {
+    assert(Logistic(1, 2, 3).isInstanceOf[Logistic])
+    assert(Logistic(1, 0, 3).isInstanceOf[Logistic])
+    assert(Logistic(1, 2, 0).isInstanceOf[Logistic])
+  }
+
+  "the derivative of Logistic" should "equal the differential of Logistic" in {
+    val logistic = Logistic(1, 2, 3)
+    assert(logistic.derivative(0) - logistic.differential(0) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(100) - logistic.differential(100) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(-100) - logistic.differential(-100) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(200) - logistic.differential(200) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(-200) - logistic.differential(-200) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(10) - logistic.differential(10) <= NumericalConstants.PRECISION)
+    assert(logistic.derivative(5) - logistic.differential(5) <= NumericalConstants.PRECISION)
+  }
+
+  "the integrate of Logistic" should "equal to integralSimson of Logistic" in {
+    val logistic = Logistic(1, 2, 3)
+    assert(logistic.integrateRomberg(0, 100) - logistic.integrate(0, 100) <= 1e-8)
+    //assert(logistic.integrateTrapezoid(0, 100) - logistic.integrate(0, 100) <= 1e-2)
+    //assert(logistic.integrateMidPoint(0, 100) - logistic.integrate(0, 100) <= 1e-2)
+    assert(logistic.integrateSimpson(0, 100) - logistic.integrate(0, 100) <= 1e-15)
+    assert(logistic.integrateIterativeLegendreGauss(0, 100) - logistic.integrate(0, 100) <= 1e-5)
+  }
 
 }
