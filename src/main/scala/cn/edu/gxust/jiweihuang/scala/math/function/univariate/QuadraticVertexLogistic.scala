021c7b84c3682a5162b0293c4184feb67a648039
JiweiHuang <huangjiwei@gxust.edu.cn>
2018-12-04 23:46:46 +0800

    update

--- src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/QuadraticVertexLogistic.scala
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/QuadraticVertexLogistic.scala
@@ -15,18 +15,11 @@
  */
 package cn.edu.gxust.jiweihuang.scala.math.function.univariate
 
-final class QuadraticVertexLogistic(override val quadraticVertexA: Double = 1.0,
-                                       override val quadraticVertexB: Double = 0.0,
-                                       override val quadraticVertexC: Double = 0.0,
-                                       override val logisticM: Double = 1.0,
-                                       override val logisticK: Double = -1.0,
-                                       override val logisticX0: Double = 0.0,
-                                       override val quadraticVertexLogisticD: Double = 0.0)
-  extends TQuadraticVertexLogistic {
-
-}
-
-
-object QuadraticVertexLogistic {
-
-}
+final case class QuadraticVertexLogistic(override val quadraticVertexA: Double = 1.0,
+                                         override val quadraticVertexB: Double = 0.0,
+                                         override val quadraticVertexC: Double = 0.0,
+                                         override val logisticM: Double = 1.0,
+                                         override val logisticK: Double = -1.0,
+                                         override val logisticX0: Double = 0.0,
+                                         override val quadraticVertexLogisticD: Double = 0.0)
+  extends TQuadraticVertexLogistic
--- src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/TQuadraticVertexLogistic.scala
+++ src/main/scala/cn/edu/gxust/jiweihuang/scala/math/function/univariate/TQuadraticVertexLogistic.scala
@@ -18,12 +18,14 @@ package cn.edu.gxust.jiweihuang.scala.math.function.univariate
 import cn.edu.gxust.jiweihuang.scala.math.function.{TUnivariateDerivativeFunction, TUnivariateDifferentiableFunction, TUnivariateIntegrableFunction, TUnivariateIntegralFunction}
 import org.hipparchus.analysis.differentiation.DerivativeStructure
 
+import scala.math._
+
 trait TQuadraticVertexLogistic extends TUnivariateDifferentiableFunction
   with TUnivariateIntegrableFunction
   with TUnivariateDerivativeFunction with TUnivariateIntegralFunction {
-  override val formula: String
-  override val dformula: String
-  override val iformula: String
+  override val formula: String = s"($quadraticVertexA*pow(x-$quadraticVertexB,2)+$quadraticVertexC)*$logisticM / (1 + exp(-$logisticK*(x-$logisticX0))) + $quadraticVertexLogisticD"
+  override val dformula: String = s"2 * $quadraticVertexA * (x - $quadraticVertexB) * $logisticM / (1 + exp(-$logisticK * (x - $logisticX0))) + ($logisticM * $logisticK * exp(-$logisticK * (x - $logisticX0))) / pow((1 + exp(-$logisticK * (x - $logisticX0))), 2) * $quadraticVertexA * pow(x - $quadraticVertexB, 2) + $quadraticVertexC +$quadraticVertexLogisticD"
+  override val iformula: String = s"$quadraticVertexLogisticD * x + $quadraticVertexA * $logisticM * pow(x, 3) / 3 - 2 * $quadraticVertexA * $quadraticVertexB * $logisticM * x * log(1 + exp($logisticK * (x - $logisticX0))) / $logisticK + $quadraticVertexA * pow($quadraticVertexB, 2) * $logisticM * log(exp($logisticK * x) + exp($logisticK * $logisticX0)) / $logisticK + $quadraticVertexC * $logisticM * log(exp($logisticK * x) + exp($logisticK * $logisticX0)) / $logisticK + $quadraticVertexA * $logisticM * pow(x, 2) * log(1 + exp($logisticK * ($logisticX0 - x))) / $logisticK"
   val quadraticVertexA: Double
   val quadraticVertexB: Double
   val quadraticVertexC: Double
@@ -34,12 +36,22 @@ trait TQuadraticVertexLogistic extends TUnivariateDifferentiableFunction
   val logistic: Logistic = Logistic(logisticM, logisticK, logisticX0)
   val quadraticVertexLogisticD: Double
 
-  override def derivative(x: Double): Double
+  override def derivative(x: Double): Double = quadraticVertex.derivative(x) * logistic.value(x) + logistic.derivative(x) * quadraticVertex.value(x)
 
-  override def integrate(x: Double): Double
+  /**
+    * omitted the Polylog part.
+    *
+    * @param x independent variable.
+    * @return the integral value.
+    */
+  override def integrate(x: Double): Double = quadraticVertexLogisticD * x + quadraticVertexA * logisticM * pow(x, 3) / 3 -
+    2 * quadraticVertexA * quadraticVertexB * logisticM * x * log(1 + exp(logisticK * (x - logisticX0))) / logisticK +
+    quadraticVertexA * pow(quadraticVertexB, 2) * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
+    quadraticVertexC * logisticM * log(exp(logisticK * x) + exp(logisticK * logisticX0)) / logisticK +
+    quadraticVertexA * logisticM * pow(x, 2) * log(1 + exp(logisticK * (logisticX0 - x))) / logisticK
 
-  override def value(derivativeStructure: DerivativeStructure): DerivativeStructure
+  override def value(ds: DerivativeStructure): DerivativeStructure = quadraticVertex.value(ds).multiply(logistic.value(ds)).add(quadraticVertexLogisticD)
 
-  override def value(v: Double): Double
+  override def value(x: Double): Double = logistic.value(x) * quadraticVertex.vertex(x) + quadraticVertexLogisticD
 }
 
